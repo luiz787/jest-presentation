93905f9593cb8a522019a8bd32cc9c227f437500
Luiz Berto <diasbertoluiz@gmail.com>
2021-05-06 20:50:30 -0300

    create component folder

--- sample-app/src/components/App.tsx
+++ sample-app/src/components/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import "./App.css";
+import "../App.css";
 import UsernameForm from "./UsernameForm";
 import Welcome from "./Welcome";
 
