74a0da2467604420c341163a6a5322ab8824bccb
Luiz Berto <diasbertoluiz@gmail.com>
2021-05-07 10:40:09 -0300

    jest-watch-select-projects

--- sample-node-app/jest.config.js
+++ sample-node-app/jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    watchPlugins: ['jest-watch-select-projects'],
     projects: [
         '<rootDir>/src/calculator/jest.config.js',
         '<rootDir>/src/github/jest.config.js'
