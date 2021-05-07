5d5c95c98f6d02b0d87727c0fb1c3549ec45c97d
Luiz Berto <diasbertoluiz@gmail.com>
2021-05-07 08:54:14 -0300

    custom assertion

--- sample-node-app/src/github/githubRepository.test.js
+++ sample-node-app/src/github/githubRepository.test.js
@@ -3,6 +3,25 @@ const { getAmountOfRepositories } = require("./githubRepository.js");
 
 jest.mock("axios");
 
+expect.extend({
+    toHaveBeenCalledOnce(received) {
+        const calls = received.mock.calls.length
+        const pass = calls === 1;
+
+        if (pass) {
+            return {
+                message: () => `expected amount of calls to be different than one`,
+                pass: true,
+            };
+        } else {
+            return {
+                message: () => `expected only one call, got ${calls} calls instead`,
+                pass: false,
+            };
+        }
+    }
+});
+
 describe("GithubRepository tests", () => {
   beforeEach(() => {
     axios.get.mockResolvedValueOnce({
@@ -29,7 +48,11 @@ describe("GithubRepository tests", () => {
 
   test("Checks if axios.get is only called once", (done) => {
     getAmountOfRepositories("luiz787").then((data) => {
-      expect(axios.get).toHaveBeenCalledTimes(1);
+        // DO
+        expect(axios.get).toHaveBeenCalledOnce();
+
+        // DONT
+        // expect(axios.get.mock.calls.length == 2).toBe(true);
 
       done();
     });
