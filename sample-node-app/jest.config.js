ce6aca583561f97644be2404a818d360d6278442
Luiz Berto <diasbertoluiz@gmail.com>
2021-05-07 11:30:01 -0300

    sonar

--- sample-node-app/jest.config.js
+++ sample-node-app/jest.config.js
@@ -3,11 +3,13 @@ module.exports = {
     "jest-watch-select-projects",
     "jest-watch-typeahead/filename",
     "jest-watch-typeahead/testname",
-    "jest-runner-eslint/watch-fix"
+    "jest-runner-eslint/watch-fix",
   ],
   projects: [
     "<rootDir>/src/calculator/jest.config.js",
     "<rootDir>/src/github/jest.config.js",
-    "<rootDir>/jest-eslint.config.js"
+    "<rootDir>/jest-eslint.config.js",
   ],
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  testResultsProcessor: "jest-sonar-reporter",
 };
