ec199e0e56d80fa319a9b699f97959f2652c15bc
Luiz Berto <diasbertoluiz@gmail.com>
2021-05-07 08:43:22 -0300

    async testing

--- sample-node-app/src/github/githubRepository.test.js
+++ sample-node-app/src/github/githubRepository.test.js
@@ -4,21 +4,20 @@ const { getAmountOfRepositories } = require("./githubRepository.js");
 jest.mock("axios");
 
 describe("GithubRepository tests", () => {
-  test("Checks if axios.get is called with the expected username", () => {
-    axios.get.mockResolvedValueOnce(() => ({
+  test("Checks if axios.get is called with the expected username", (done) => {
+    axios.get.mockResolvedValueOnce({
       status: 200,
       data: {
         length: 1,
       },
-    }));
+    });
 
-    // FIXME: teste bugado
     getAmountOfRepositories("luiz787").then((data) => {
-      expect(axios.get).not.toHaveBeenCalledWith(
+      expect(axios.get).toHaveBeenCalledWith(
         "https://api.github.com/users/luiz787/repos"
       );
 
-      // TODO: adicionar done
+      done();
     });
   });
 });
